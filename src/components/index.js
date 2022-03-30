6517e1d558b92a79341092ec5d5731ed28108fe1
Дмитрий Гринь <dzmitry.hryn@gmail.com>
2022-03-30 12:20:56 +0300

    sorting component, routing

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as Button} from './Button';
 export {default as Categoties} from './Categoties';
 export {default as Header} from './Header';
+export {default as SortPopup} from './SortPopup';
