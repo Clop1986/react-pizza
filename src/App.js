468c4e5ac1c2dace81cb2d21f9e1a513c48b572e
Дмитрий Гринь <dzmitry.hryn@gmail.com>
2022-03-31 16:28:02 +0300

    Added a database, made a withdrawal of cards from the database

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import axios from 'axios';
+
 import { Route, Routes } from 'react-router-dom';
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Home items={pizzas} />} />
           <Route exact path="/cart" element={<Cart />} />
         </Routes>
       </div>
